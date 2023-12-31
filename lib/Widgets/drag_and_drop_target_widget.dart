4af95202f12d44fc59ebf91ee4c5dda19aac02d3
Y0L042 <vlamfletcher@gmail.com>
2023-12-31 17:24:47 +0200

    refactor(DragAndDrop Widget): Refactored and fixed DragAndDrop Widget. Status: wip
    The widget has a minimum height. A fractionalbox sits within this constraint.
    
    Broken: Refactor in progress.
    
    Workstation: JJDesktop

--- lib/Widgets/drag_and_drop_target_widget.dart
+++ lib/Widgets/drag_and_drop_target_widget.dart
@@ -2,23 +2,40 @@
 import 'package:flutter/material.dart';
 
 class DragAndDropTarget extends StatelessWidget {
-  const DragAndDropTarget({super.key});
+  final double minHeight;
+  final double widthFactor;
+  final double heightFactor;
+
+  const DragAndDropTarget({
+    super.key,
+    this.minHeight = 100,
+    this.widthFactor = 0.9, // 50% of the parent's width
+    this.heightFactor = 0.9, // 10% of the parent's height
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const FractionallySizedBox(
-        widthFactor: .75,
-        heightFactor: .15,
-        child: ColoredBox(
-          color: Colors.lightBlue,
-          child: Center(
-            child: Text(
-              "Drop folders here.",
-              textAlign: TextAlign.center,
-            ),
-          ),
+    return SizedBox(
+      height: minHeight,
+      child: Center(
+        child: FractionallySizedBox(
+          widthFactor: 0.9,
+          heightFactor: 0.9,
+          child: _buildContent()
+        ),
+      )
+    );
+  }
+
+  Widget _buildContent() {
+    return const DecoratedBox(
+      decoration: BoxDecoration(
+        color: Colors.blue,
+      ),
+      child: Center(
+        child: Text(
+          "Drop folders here.",
+          textAlign: TextAlign.center,
         ),
       ),
     );
